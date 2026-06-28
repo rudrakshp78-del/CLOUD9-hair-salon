af39f6c9ed50ed74f17546686cf7baea944d12ff
Rudraksh Paliwal <rudrakshp78@gmail.com>
2026-06-28 15:14:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,11 @@ const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
 menuToggle.addEventListener("click", () => {
-
     navLinks.classList.toggle("active");
 
-    if (navLinks.classList.contains("active")) {
+    if(navLinks.classList.contains("active")){
         menuToggle.innerHTML = "✖";
     } else {
         menuToggle.innerHTML = "☰";
     }
-
-});
-
-// Close menu after clicking a link
-const links = document.querySelectorAll("#navLinks a");
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
-        menuToggle.innerHTML = "☰";
-    });
 });
