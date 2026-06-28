b6cd8e7c7a718651a0b6f732da1770ef0764a505
Rudraksh Paliwal <rudrakshp78@gmail.com>
2026-06-28 15:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("onclick", () => {
     navLinks.classList.toggle("active");
 
     if(navLinks.classList.contains("active")){
